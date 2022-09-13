c39d05cf29b9fb9e6e9f0ae4825a10f59077f664
Jürgen Mikk <jurgen.mikk@khk.ee>
2022-09-13 19:58:36 +0300

    As a user I can see a list of users [Delivers #183266039]

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,60 @@
-import React from 'react';
-
-import './App.css';
+import React, { useEffect } from 'react';
+import axios from "axios";
 
 function App() {
-  return (
-    <div className="">
 
-    </div>
+   interface userData {
+        id: number,
+        first_name: string,
+        last_name: string,
+        avatar: string,
+  }
+
+  const [users, setUsers ] = React.useState<userData[]>([]);
+
+  useEffect(() => {
+    handleGET();
+  }, []);
+
+  const handleGET = () => {
+    axios.get("https://reqres.in/api/users")
+        .then(response => {
+          setUsers(users.concat(response.data.data));
+        })
+  }
+
+  return (
+      <div className="h-screen w-full">
+        <div className="w-2/3 h-full flex items-center justify-center m-auto overflow-x-auto overflow-y-auto relative shadow-md sm:rounded-lg">
+          <table className="w-full align-center text-center text-lg text-left text-gray-500 justify-center mb-20">
+            <thead className="text-xs text-gray-800 uppercase bg-gray-50">
+                <tr>
+                    <th className="py-10 px-6 text-xl">Id</th>
+                    <th className="py-10 px-6 text-xl">First_name</th>
+                    <th className="py-10 px-6 text-xl">Last_name</th>
+                    <th className="py-10 px-6 text-xl">Avatar</th>
+                </tr>
+            </thead>
+            {users.map((data, index : number) => {
+              return (
+                <tbody key={index}>
+                    <tr className="bg-white border-b">
+                        <td>{data.id}</td>
+                        <td>{data.first_name}</td>
+                        <td>{data.last_name}</td>
+                        <td>
+                        <img className="w-20 h-20 rounded-full mr-auto ml-auto mb-2"
+                           src={data.avatar}
+                           alt="profile"
+                        />
+                        </td>
+                    </tr>
+                </tbody>
+                )
+            })}
+          </table>
+        </div>
+      </div>
   );
 }
 
