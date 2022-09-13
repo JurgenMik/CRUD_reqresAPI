8fa16f9aa748f7877fdac813cb940bab2318feff
Jürgen Mikk <jurgen.mikk@khk.ee>
2022-09-13 19:27:38 +0300

    Implemented & configured TailwindCSS

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+module.exports = {
+  content: ["./src/**/*.{html,tsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        'sans' : ['League Spartan', ...defaultTheme.fontFamily.sans],
+      }
+    },
+  },
+  plugins: [],
+}
